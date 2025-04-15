9c26af76c1d0d8f5f8c6c574c0ac0533a2ca6f8c
jenilghori <110442926+jenilghori@users.noreply.github.com>
2025-04-15 18:50:47 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -57,8 +57,8 @@ export const config = {
             args: [
               '--headless',
               '--disable-gpu',
-      // '--user-data-dir=/some/path' ← remove or make this unique
-    ]
+                      // '--user-data-dir=/some/path' ← remove or make this unique
+                ]}
         
     }],
 
