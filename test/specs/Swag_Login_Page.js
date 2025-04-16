f53d4294100c08ebd10b0c04a847c3fd47e2fef7
jenilghori <110442926+jenilghori@users.noreply.github.com>
2025-04-16 10:46:45 +0530

    Update Swag_Login_Page.js

--- test/specs/Swag_Login_Page.js
+++ test/specs/Swag_Login_Page.js
@@ -19,29 +19,29 @@ describe('Swag Lab Login Test', async () => {
         await expect(browser).toHaveUrl(expect.stringContaining('inventory'))
 
     })
-    // it('should show error with invalid credentials', async () => {
+    it('should show error with invalid credentials', async () => {
 
-    //     await LoginPage.login(loginData.invalid_User.username, loginData.invalid_User.password)
-    //     //Verify error msg   
-    //     const errorMsg = await LoginPage.getErrorMsg()
-    //     await expect(errorMsg).toContain(ERROR_MSG)
+        await LoginPage.login(loginData.invalid_User.username, loginData.invalid_User.password)
+        //Verify error msg   
+        const errorMsg = await LoginPage.getErrorMsg()
+        await expect(errorMsg).toContain(ERROR_MSG)
 
-    // })
-    // it('should login only username with valid credentials',async()=>{
-    //     await LoginPage.username.setValue(loginData.valid_User.username)
-    //     await LoginPage.loginButton.click()
+    })
+    it('should login only username with valid credentials',async()=>{
+        await LoginPage.username.setValue(loginData.valid_User.username)
+        await LoginPage.loginButton.click()
 
-    //      //Verify error msg   
-    //     const errorMsg = await LoginPage.getErrorMsg()
-    //     await expect(errorMsg).toContain(USERNAME_ERROR_MSG)
-    // })
-    // it('should login only password with valid credentials',async()=>{
-    //     await LoginPage.password.setValue(loginData.valid_User.password)
-    //     await LoginPage.buttoIsDisplayed("#login-button")
-    //      //Verify error msg   
-    //     const errorMsg = await LoginPage.getErrorMsg()
-    //     await expect(errorMsg).toContain(PASSWORD_ERROR_MSG)
-    // })
+         //Verify error msg   
+        const errorMsg = await LoginPage.getErrorMsg()
+        await expect(errorMsg).toContain(USERNAME_ERROR_MSG)
+    })
+    it('should login only password with valid credentials',async()=>{
+        await LoginPage.password.setValue(loginData.valid_User.password)
+        await LoginPage.buttoIsDisplayed("#login-button")
+         //Verify error msg   
+        const errorMsg = await LoginPage.getErrorMsg()
+        await expect(errorMsg).toContain(PASSWORD_ERROR_MSG)
+    })
 
 
 })
